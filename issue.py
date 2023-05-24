486e798064b5aceb6d326e1cc42f2bfdfbe38d5a
Meet <mdc8464@gmail.com>
2023-05-24 09:33:33 -0400

    changed

--- issue.py
+++ issue.py
@@ -6,18 +6,18 @@ import basis_set_exchange as bse
 from FcMole import FcM, FcM_like
 from AP_class import APDFT_perturbator as AP
 
-# doesn't work:
-NN = gto.M(atom= f"N 0 0 0; N 0 0 2.1",unit="Bohr",basis=bse.get_basis('unc-ccpvdz',fmt='guassian09', elements=[5,6,7,8,9]))
+# # doesn't work:
+NN = gto.M(atom= f"N 0 0 0; N 0 0 2.1",unit="Bohr",basis='unc-ccpvdz')
 
 # works
-mol = gto.M(
-    atom = '''N 0 0 0; N 0 0 2''',
-    basis =
-    {'N': 'unc-ccpvdz', # prefix "unc-" will uncontract the ccpvdz basis.
-                                # It is equivalent to assigning
-                                #   'O': gto.uncontract(gto.load('ccpvdz', 'O')),
-             'H': 'unc-ccpvdz'  # H1 H2 will use the same basis ccpvdz
-            }
-)
+# mol = gto.M(
+#     atom = '''N 0 0 0; N 0 0 2''',
+#     basis =
+#     {'N': 'unc-ccpvdz', # prefix "unc-" will uncontract the ccpvdz basis.
+#                                 # It is equivalent to assigning
+#                                 #   'O': gto.uncontract(gto.load('ccpvdz', 'O')),
+#              'H': 'unc-ccpvdz'  # H1 H2 will use the same basis ccpvdz
+#             }
+# )
 
 
