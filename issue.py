2c2794ba781479595424928d65b37f1039bf49d3
Meet <mdc8464@gmail.com>
2023-06-01 01:27:17 -0400

    error in issue.py

--- issue.py
+++ issue.py
@@ -6,18 +6,24 @@ import basis_set_exchange as bse
 from FcMole import FcM, FcM_like
 from AP_class import APDFT_perturbator as AP
 
-# # doesn't work:
-NN = gto.M(atom= f"N 0 0 0; N 0 0 2.1",unit="Bohr",basis='unc-ccpvdz')
+# creating a NNH molecule
+ex = gto.M(atom= f"N 0 0 0; N 0 0 2.1; H 0 0 {2.1+0.94}",unit="Bohr",charge=1,basis='unc-ccpvdz')
+# the below should ideally create SiH molecule
+new_ex = FcM_like(ex,fcs=[[0,1],[-7,7]])
+mf_mol=scf.RKS(new_ex)
+mf_mol.xc="PBE0"
+Te_mol=mf_mol.scf(dm0=mf_mol.init_guess_by_1e())
 
-# works
-# mol = gto.M(
-#     atom = '''N 0 0 0; N 0 0 2''',
-#     basis =
-#     {'N': 'unc-ccpvdz', # prefix "unc-" will uncontract the ccpvdz basis.
-#                                 # It is equivalent to assigning
-#                                 #   'O': gto.uncontract(gto.load('ccpvdz', 'O')),
-#              'H': 'unc-ccpvdz'  # H1 H2 will use the same basis ccpvdz
-#             }
-# )
+# this should ideally be electron energy of SiH molecule
+elec_energy = round(mf_mol.energy_elec()[0],3)
 
+# calculating explicitly energy of SiH molecule
+mol_ac = gto.M(atom= f"Si 0 0 2.1; H 0 0 {2.1+0.94}",unit="Bohr",charge=1,basis='unc-ccpvdz')
+mf_mol_ac=scf.RKS(mol_ac)
+mf_mol_ac.xc="PBE0"
+Te_mol_ac=mf_mol_ac.scf(dm0=mf_mol_ac.init_guess_by_1e())
 
+# this should ideally be electron energy of SiH molecule
+actual_elec_energy = round(mf_mol_ac.energy_elec()[0],3)
+
+print(elec_energy == actual_elec_energy)
